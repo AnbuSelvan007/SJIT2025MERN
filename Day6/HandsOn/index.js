c8a0a6bbc75a7ad66fbb0f40300b65322b217703
AnbuSelvan007 <anbuselvan7980@gmail.com>
2025-02-14 13:33:39 +0530

    Day6-FN

---
+++ Day6/HandsOn/index.js
@@ -0,0 +1,31 @@
+const name="Anbuselvan"
+console.log(name)
+
+let a = 10, b = 3;
+
+console.log(a + b); // Addition → 13
+console.log(a - b); // Subtraction → 7
+console.log(a * b); // Multiplication → 30
+console.log(a / b); // Division → 3.3333
+console.log(a % b); // Modulus (Remainder) → 1
+console.log(a ** b); // Exponentiation (a^b) → 1000
+
+let x = 5;
+x += 3;  // Equivalent to x = x + 3 → x = 8
+x -= 2;  // x = x - 2 → x = 6
+x *= 2;  // x = x * 2 → x = 12
+x /= 3;  // x = x / 3 → x = 4
+x %= 3;  // x = x % 3 → x = 1
+
+console.log(5 == "5");   // true (Checks only value)
+console.log(5 === "5");  // false (Checks value & type)
+console.log(5 != "5");   // false
+console.log(5 !== "5");  // true
+console.log(10 > 5);     // true
+console.log(10 < 5);     // false
+console.log(10 >= 10);   // true
+console.log(10 <= 9);    // false
+
+console.log(5 & 1);  // AND → 5 (101) & 1 (001) = 1
+console.log(5 | 1);  // OR  → 5 (101) | 1 (001) = 5
+console.log(5 ^ 1);  // XOR → 5 (101) ^ 1 (001) = 4
