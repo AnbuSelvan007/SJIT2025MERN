863f5020c28dd74ba54c04ffdf62f391b52ee08d
AnbuSelvan007 <anbuselvan7980@gmail.com>
2025-02-20 08:19:19 +0530

    Day-9 Task

--- sjitFrontend/src/components/function/Login.jsx
+++ sjitFrontend/src/components/function/Login.jsx
@@ -2,9 +2,31 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>
-      Login
-    </div>
+    <>
+        <center>
+     <form  style={{marginTop:"20vh"}}>
+        <table cellpadding="10px">
+          
+          <tr>
+            <th>
+              <label for="">Email</label>
+            </th>
+            <td><input type="email" /></td>
+          </tr>
+         
+          <tr>
+            <th>
+              <label for="">Password</label>
+            </th>
+            <td><input type="text" /></td>
+          </tr>
+        </table>
+        <button>Login</button>
+        <p>or</p>
+        <button>Don't Have an Account</button>
+      </form>
+      </center>
+    </>
   )
 }
 
--- sjitFrontend/src/components/function/SignUp.jsx
+++ sjitFrontend/src/components/function/SignUp.jsx
@@ -1,10 +1,64 @@
 import React from 'react'
-
+import './SignUp.css'
 function SignUp() {
   return (
-    <div>
-      SignUP
-    </div>
+    <>
+    <center>
+     <form  style={{marginTop:"20vh"}}>
+        <table cellpadding="10px">
+          <tr>
+            <th>
+              <label for="">First Name</label>
+            </th>
+            <td><input type="text" /></td>
+          </tr>
+          <tr>
+            <th>
+              <label for="">Last Name</label>
+            </th>
+            <td><input type="text" /></td>
+          </tr>
+          <tr>
+            <th>
+              <label for="">Email</label>
+            </th>
+            <td><input type="email" /></td>
+          </tr>
+          <tr>
+            <th>
+              <label for="">D.O.B</label>
+            </th>
+            <td><input type="date" /></td>
+          </tr>
+          <tr>
+            <th>
+              <label for="">Password</label>
+            </th>
+            <td><input type="text" /></td>
+          </tr>
+          <tr>
+            <th>
+              <label for="">verify password</label>
+            </th>
+            <td><input type="text" /></td>
+          </tr>
+          <tr>
+            <th>
+              <label for="">Gender</label>
+            </th>
+            <td>
+              <input type="radio" name="gender" value="male" /> Male
+              <input type="radio" name="gender" value="female" /> Female
+            </td>
+          </tr>
+        </table>
+        <button>SignUP</button>
+        <p>or</p>
+        <button>Aleardy Have an Account</button>
+      </form>
+
+      </center>
+    </>
   )
 }
 
